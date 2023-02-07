de7d69d9078f1684bdf35fea6a8fb7e16ef2a359
lin-mt <lin-mt@outlook.com>
2023-02-07 13:57:47 +0800

    feat: add dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,20 +20,46 @@ repositories {
 
 var queryDslVersion = "5.0.0"
 var jakartaPersistenceVersion = "3.1.0"
+var byteBuddyVersion = "1.12.23"
+var commonsLang3Version = "3.12.0"
+var commonsCollections4Version = "4.4"
+var googleGuavaVersion = "31.1-jre"
+var jacksonModuleJakartaXmlbindAnnotationsVersion = "2.14.2"
+var mapstructVersion = "1.5.3.Final"
+var okhttpVersion = "4.10.0"
+var blazePersistenceVersion = "1.6.8"
+var swaggerParserVersion = "2.1.11"
+var minioVersion = "8.5.1"
 
 dependencies {
-    implementation("com.querydsl:querydsl-core:${queryDslVersion}")
-    implementation("com.querydsl:querydsl-jpa:${queryDslVersion}:jakarta")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("io.minio:minio:${minioVersion}")
+    implementation("org.mapstruct:mapstruct:${mapstructVersion}")
+    implementation("com.google.guava:guava:${googleGuavaVersion}")
+    implementation("net.bytebuddy:byte-buddy:${byteBuddyVersion}")
+    implementation("com.squareup.okhttp3:okhttp:${okhttpVersion}")
+    implementation("com.querydsl:querydsl-jpa:${queryDslVersion}:jakarta")
+    implementation("com.querydsl:querydsl-core:${queryDslVersion}")
+    implementation("net.bytebuddy:byte-buddy-agent:${byteBuddyVersion}")
+    implementation("org.apache.commons:commons-lang3:${commonsLang3Version}")
+    implementation("io.swagger.parser.v3:swagger-parser:${swaggerParserVersion}")
+    implementation("org.apache.commons:commons-collections4:${commonsCollections4Version}")
+    implementation("com.fasterxml.jackson.module:jackson-module-jakarta-xmlbind-annotations:${jacksonModuleJakartaXmlbindAnnotationsVersion}")
     compileOnly("org.projectlombok:lombok")
+    compileOnly("com.blazebit:blaze-persistence-core-api-jakarta:${blazePersistenceVersion}")
+    compileOnly("com.blazebit:blaze-persistence-integration-querydsl-expressions-jakarta:${blazePersistenceVersion}")
     runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("com.blazebit:blaze-persistence-core-impl-jakarta:${blazePersistenceVersion}")
+    runtimeOnly("com.blazebit:blaze-persistence-integration-hibernate-6.0:${blazePersistenceVersion}")
     annotationProcessor("org.projectlombok:lombok")
     annotationProcessor("com.querydsl:querydsl-apt:${queryDslVersion}:jakarta")
     annotationProcessor("jakarta.persistence:jakarta.persistence-api:${jakartaPersistenceVersion}")
+    annotationProcessor("org.mapstruct:mapstruct-processor:${mapstructVersion}")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
